269b638293bc2ebcff5d802ea0beb232a40b4938
Isaac Collier <isaacjcollier@gmail.com>
2016-05-21 17:22:33 -0600

    Hamming Exercism Exercise

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,9 +1,18 @@
-var Hamming = function() {
+var Hamming = function(){}
+
+Hamming.prototype.compute = function(dna1, dna2){
+  var count = 0
+  dna1.split('')
+  dna2.split('')
+  if(dna1.length != dna2.length){
+throw new Error('DNA strands must be of equal length.')
+}
+  for( i = 0 ;i < dna1.length ; i++){
+    if(dna1[i] != dna2[i] ){
+      count += 1
+    }
+  }
+  return count
 }
 
-Hamming.prototype.compute = function(strandOne, strandTwo){
-  for ( ; ; )
-
-}
-
-module.exports = Hamming
+module.exports = Hamming;

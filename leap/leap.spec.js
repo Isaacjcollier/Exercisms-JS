b2a442207615c08cbd04f7ad86c9b2edb5d0e571
Isaac Collier <isaacjcollier@gmail.com>
2016-05-23 11:39:01 -0600

    rna-transcription exercism 100% complete

--- leap/leap.spec.js
+++ leap/leap.spec.js
@@ -6,7 +6,7 @@ describe('Leap year', function() {
     var year = new Year(2015);
     expect(year.isLeap()).toBe(false);
   });
-
+  
   xit('is introduced every 4 years to adjust about a day', function() {
     var year = new Year(2016);
     expect(year.isLeap()).toBe(true);
---
+++ rna-transcription/rna-transcription.js
@@ -0,0 +1,18 @@
+var DnaTranscriber = function(){}
+
+DnaTranscriber.prototype.toRna = function(strand){
+  console.log(strand)
+  var compliment ={'C':'G', 'G' : 'C', 'A' : 'U', 'T' : 'A'
+  }
+    function translate(transform){
+      return compliment[transform];
+    }
+    return strand.split('').map(translate).join('')
+
+}
+module.exports = DnaTranscriber
+
+// G = C
+// C = G
+// A = U
+// T = A

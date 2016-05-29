8017e7403b572f395228e7a55442d31684916dc8
Isaac Collier <isaacjcollier@gmail.com>
2016-05-28 21:57:53 -0600

    word-count exercism exercise

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,8 +1,29 @@
 var Words = function (){}
 
 Words.prototype.count = function(words){
-  words.split('')
-  console.log(words)
+  var obj = {}
+  // ['one', 'of', 'each'] (.+)
+  var seperatedWords = words.trim().toLowerCase().split(/\s+|\n+|\t+/)
+
+  for ( i = 0 ; seperatedWords.length > i ; i++ ){
+   var singleWord = seperatedWords[i]
+   var counter = 0
+
+   for(j = 0; seperatedWords.length > j; j++){
+     if(singleWord === seperatedWords[j]){
+       counter++
+     }
+   }
+   obj[singleWord] = counter
+  }
+  return obj
 }
 
 module.exports = Words
+// - separatedWords => ['one', 'of', 'each']
+// - count = 0
+//
+// loop ['one', 'of', 'each'] do |var|
+//   var === 'one'
+//   loop ['one', 'of', 'each'] do |othervar|
+//     if var == othervar then count++
